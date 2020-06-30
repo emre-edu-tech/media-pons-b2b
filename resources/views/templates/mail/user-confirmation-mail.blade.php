d6e5f6edd062d4827703680447d8ac10ec34a847
Emre Kayikcilar <emreebrukayikcilar@gmail.com>
2020-06-30 12:28:38 +0300

    User mail content updated

--- resources/views/templates/mail/user-confirmation-mail.blade.php
+++ resources/views/templates/mail/user-confirmation-mail.blade.php
@@ -1,2 +1,3 @@
-<p>Your user credentials are as follows: </p>
+<p>Ihre Benutzerdaten sind wie folgt: </p>
+<p><a href="{{ url('/login') }}" target="_blank">{{ url('/login') }}</a></p>
 <p>username: {{ $user->email }}<br>password: {{ $password }}</p>
