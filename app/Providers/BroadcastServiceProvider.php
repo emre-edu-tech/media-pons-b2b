cd4e213c523e2323db8f2e2ce2ef70ebe5a3630b
Emre Kayikcilar <emreebrukayikcilar@gmail.com>
2020-07-04 10:26:57 +0300

    Routes updated

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -15,9 +15,8 @@ class BroadcastServiceProvider extends ServiceProvider
     public function boot()
     {
         // Broadcast::routes(['middleware' => ['web']]);
-        // Broadcast::routes();
+        Broadcast::routes();
         // Broadcast::routes(['middleware' => ['auth:api']]);
-        Broadcast::routes(['prefix' => 'api', 'middleware' => 'auth:api']);
 
         require base_path('routes/channels.php');
     }
